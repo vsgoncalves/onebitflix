03630014b8aaa0b08d49812e5057410e91c2d8ca
Vinicius Goncalves <vinicius.souza.goncalves@gmail.com>
2023-08-31 12:07:42 -0300

    migrations de episodios

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,9 +1,10 @@
 // src/adminjs/resources/index.ts
 
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
+import { episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
@@ -13,6 +14,11 @@ export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Course,
     options: courseResourceOptions
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions
+  
   }
 ]
 
