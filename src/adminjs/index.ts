138a50d7a4606bdeb6fa6101debde5663c656204
Vinicius Goncalves <vinicius.souza.goncalves@gmail.com>
2023-09-01 16:46:57 -0300

    Dashboard personalizado

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -4,8 +4,10 @@ import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
 import { sequelize } from '../database';
 import { adminJsResources } from './resources'
-import { User } from '../models';
+import { Category, Course, Episode, User } from '../models'
 import bcrypt from 'bcrypt'
+import { locale } from './locale';
+
 
 AdminJs.registerAdapter(AdminJsSequelize)
 
@@ -33,7 +35,24 @@ export const adminJs = new AdminJs({
 	      hoverBg: '#151515',
       }
     }
-  }
+  },
+  locale: locale,
+  dashboard: {
+    component: AdminJs.bundle('./components/Dashboard'),
+    handler: async (req, res, context) => {
+      const courses = await Course.count()
+      const episodes = await Episode.count()
+      const category = await Category.count()
+      const standardUsers = await User.count({ where: { role: 'user' } })
+
+      res.json({
+        'Cursos': courses,
+        'Episódios': episodes,
+        'Categorias': category,
+        'Usuários': standardUsers
+      })
+    },
+  },
 })
 
 //export const adminJsRouter = AdminJsExpress.buildRouter(adminJs)
