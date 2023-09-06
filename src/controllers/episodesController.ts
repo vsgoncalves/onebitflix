d7780ddf0dcffb8aa2a45b997a30f8eb712c7658
Vinicius Goncalves <vinicius.souza.goncalves@gmail.com>
2023-09-06 17:06:37 -0300

    problema na Streaming de vídeos na API resolvido

--- src/controllers/episodesController.ts
+++ src/controllers/episodesController.ts
@@ -1,66 +1,24 @@
 // src/controllers/episodesController.ts
 
 import { Request, Response } from 'express'
-import fs from 'fs'
-import path from 'path'
-
+import { episodeService } from '../services/episodeService'
 
 export const episodesController = {
   // GET /episodes/stream
   stream: async (req: Request, res: Response) => {
-    const { videoUrl } = req.body
+    const { videoUrl } = req.query
+    const range = req.headers.range
 
     try {
-
-        if (typeof videoUrl !== 'string') {
-    throw new Error('videoUrl must be of type \'string\'');
-  }
-
-  const filePath = path.join(__dirname, '../../uploads', videoUrl)
-  const fileStat = fs.statSync(filePath)
-
-  const range = req.headers.range
-
-  if (range) {
-    const parts = range.replace(/bytes=/, '').split('-')
-
-    const start = parseInt(parts[0], 10)
-    const end = parts[1] ? parseInt(parts[1], 10) : fileStat.size - 1
-
-            const chunkSize = (end - start) + 1
-
-            const file = fs.createReadStream(filePath, { start, end })
-
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileStat.size}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunkSize,
-      'Content-Type': 'video/mp4',
-    }
-
-    res.writeHead(206, head)
-
-    file.pipe(res)
-
-      } else {
-
-				const head = {
-          'Content-Length': fileStat.size,
-          'Content-Type': 'video/mp4',
-        }
-
-        res.writeHead(200, head)
-
-        fs.createReadStream(filePath).pipe(res)
-
+      if (typeof videoUrl !== 'string') {
+        throw new Error('videoUrl must be of type \'string\'');
       }
 
+      episodeService.streamEpisodeToResponse(res, videoUrl, range)
     } catch (err) {
-
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
       }
-
     }
-  }
+  },
 }
