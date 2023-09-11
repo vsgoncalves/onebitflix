0e1c177769827566edde7fe01459c43aefafc7fd
Vinicius Goncalves <vinicius.souza.goncalves@gmail.com>
2023-09-11 16:53:56 -0300

    middleware de autorização

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -5,5 +5,9 @@ const secret = 'chave-jwt'
 export const jwtService = {
   signToken: (payload: string | object | Buffer, expiration: string) => {
     return jwt.sign(payload, secret, { expiresIn: expiration })
+  },
+
+  verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+    jwt.verify(token, secret, callbackfn)
   }
 }
