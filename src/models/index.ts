fcc1f8a6619cb93a5a621d8a01ad18491134e2bc
Vinicius Goncalves <vinicius.souza.goncalves@gmail.com>
2023-09-13 17:04:42 -0300

    like com erro

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,12 +5,14 @@ import { Course } from './Course'
 import { Episode } from './Episode'
 import { Favorite } from './Favorite'
 import { User } from './User'
+import { Like } from './Like'
 
 Category.hasMany(Course)
 
 Course.belongsTo(Category)
 Course.hasMany(Episode)
 Course.belongsToMany(User, { through: Favorite })
+Course.belongsToMany(User, { through: Like , as: 'likes'})
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 
 Episode.belongsTo(Course)
@@ -19,12 +21,15 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Course, { through: Like})
 User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
+
 
 export {
   Category,
   Course,
   Episode,
 	Favorite,
+  Like,
   User
 }
